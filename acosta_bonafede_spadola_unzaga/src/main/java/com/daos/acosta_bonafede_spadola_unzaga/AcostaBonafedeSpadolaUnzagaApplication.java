2aebbe0705cc9a9440227fc587a00af06dca1c1d
Mda1936cantin <matiasdavidacosta@gmail.com>
2025-11-06 16:56:21 -0300

    actualizacion de estructura

---
+++ acosta_bonafede_spadola_unzaga/src/main/java/com/daos/acosta_bonafede_spadola_unzaga/AcostaBonafedeSpadolaUnzagaApplication.java
@@ -0,0 +1,13 @@
+package com.daos.acosta_bonafede_spadola_unzaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcostaBonafedeSpadolaUnzagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcostaBonafedeSpadolaUnzagaApplication.class, args);
+	}
+
+}
---
+++ acosta_bonafede_spadola_unzaga/src/test/java/com/daos/acosta_bonafede_spadola_unzaga/AcostaBonafedeSpadolaUnzagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daos.acosta_bonafede_spadola_unzaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcostaBonafedeSpadolaUnzagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
