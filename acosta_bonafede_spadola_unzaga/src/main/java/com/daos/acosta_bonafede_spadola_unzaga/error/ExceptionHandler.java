0f95adbe7f95f5e9e2c1c8960e60670c5ff56b74
Nicolas Spadola <gamesclasic31@gmail.com>
2025-11-10 17:49:03 -0300

    initial commit

--- acosta_bonafede_spadola_unzaga/src/main/java/com/daos/acosta_bonafede_spadola_unzaga/error/ExceptionHandler.java
+++ acosta_bonafede_spadola_unzaga/src/main/java/com/daos/acosta_bonafede_spadola_unzaga/error/ExceptionHandler.java
@@ -1,20 +1,8 @@
 package com.daos.acosta_bonafede_spadola_unzaga.error;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import jakarta.servlet.http.HttpServletRequest;
-import com.daos.acosta_bonafede_spadola_unzaga.ExceptionPersonal.Excepcion;
 
 @RestControllerAdvice
 public class ExceptionHandler {
 
-   @org.springframework.web.bind.annotation.ExceptionHandler(Excepcion.class)
-   public ResponseEntity<ErrorInfo> methodArgumentNotValidException(HttpServletRequest request, Excepcion e) {
-	   int statusCode= e.getStatusCode();
-	   
-	   ErrorInfo errorInfo = new ErrorInfo(HttpStatus.BAD_REQUEST.value(), e.getMessage(), request.getRequestURI());
-       return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
-   }
 }
